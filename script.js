2c7aee41096dad3602ebb9cdccd8a3d3d25fec2c
DanielHill2005 <93940164+DanielHill2005@users.noreply.github.com>
2022-05-13 14:09:57 -0700

    most features done

--- script.js
+++ script.js
@@ -32,3 +32,49 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+function moreLess (n) {
+  switch (n){
+    case 1:
+      let dots1 = document.getElementById("dots1");
+      let moreText1 = document.getElementById("more1");
+      let btnText1 = document.getElementById("moreBtn1");
+      if (dots1.style.display === "none") {
+        dots1.style.display = "inline";
+        btnText1.innerHTML = "Read more";
+        moreText1.style.display = "none";
+      } else {
+        dots1.style.display = "none";
+        btnText1.innerHTML = "Read less";
+        moreText1.style.display = "flex";
+      }
+      break;
+    case 2:
+      let dots2 = document.getElementById("dots2");
+      let moreText2 = document.getElementById("more2");
+      let btnText2 = document.getElementById("moreBtn2");
+      if (dots2.style.display === "none") {
+        dots2.style.display = "inline";
+        btnText2.innerHTML = "Read more";
+        moreText2.style.display = "none";
+      } else {
+        dots2.style.display = "none";
+        btnText2.innerHTML = "Read less";
+        moreText2.style.display = "inline";
+      }
+      break;
+    case 3:
+      let dots3 = document.getElementById("dots3");
+      let moreText3 = document.getElementById("more3");
+      let btnText3 = document.getElementById("moreBtn3");
+      if (dots3.style.display === "none") {
+        dots3.style.display = "inline";
+        btnText3.innerHTML = "Read more";
+        moreText3.style.display = "none";
+      } else {
+        dots3.style.display = "none";
+        btnText3.innerHTML = "Read less";
+        moreText3.style.display = "inline";
+      }
+      break;
+  }
+}
